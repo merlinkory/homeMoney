0a8bc794f31ab06b9a14eec9e082142b3de59270
Merlin <merlinkory@yandex.ru>
2022-11-20 17:05:40 +0700

    initial commit

---
+++ database/migrations/2022_11_20_085302_create_costs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('costs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('coast_group_id');
+            $table->decimal('amount',2,10);
+            $table->string('currency',3);
+            $table->string('description', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('costs');
+    }
+};
---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+Vue3!!!
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
